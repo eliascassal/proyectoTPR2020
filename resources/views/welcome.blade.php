e256c87c0b68d61df2452b229b2628456661f62b
eliascassal <eliascassal@gmail.com>
2020-09-29 19:41:16 -0400

    welcome change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TECNOLOGIA DE LA PROGRAMACION EN RED 2020
                 </div>
 
                 <div class="links">
